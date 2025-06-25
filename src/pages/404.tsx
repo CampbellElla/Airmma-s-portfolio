8f3ecf8f2e6523a83dc5b49090ac273bcfe3a460
Campbell Emmanuella <campbellemmanuella17@gmail.com>
2025-06-25 16:22:06 +0100

    skills correction

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -19,7 +19,7 @@ const Page404 = () => {
     alert(JSON.stringify(inputs, null, 2));
   };
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center bg-blue-600/20 h-screen">
       <form action={submitform} className="mx-auto w-[50%] flex flex-col gap-4">
         <div className="flex flex-col">
           <label className="font-bold text-lg text-black">Email</label>
@@ -36,10 +36,10 @@ const Page404 = () => {
         </div>
         <div className="flex flex-col">
           <label className="font-bold text-lg text-black">Password</label>
-          <div className="flex items-center justify-center px-4 py-2 rounded-m outline-none border">
+          <div className="flex items-center justify-center px-4 py-2 rounded-m outline-none border bg-white">
             <input
               type={view ? "password" : "text"}
-              className="outline-none w-full"
+              className="outline-none w-full "
               placeholder="Password"
               name="password"
               onChange={(e) =>
@@ -65,7 +65,7 @@ const Page404 = () => {
           <label className="font-bold text-lg text-black">
             Confirm Password
           </label>
-          <div className="flex items-center justify-center px-4 py-2 rounded-m outline-none border">
+          <div className="flex items-center justify-center px-4 py-2 rounded-m outline-none border bg-white">
             <input
               type={view2 ? "password" : "text"}
               className="outline-none w-full"
